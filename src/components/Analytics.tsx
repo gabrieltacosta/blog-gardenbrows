4111066e54853569ab4391b9da037c63f490d606
Gabriel Costa <107140329+gabrieltacosta@users.noreply.github.com>
2026-05-03 23:17:39 -0300

    Refactor Google Analytics script injection

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -27,15 +27,14 @@ export default function Analytics() {
   // Se tem consentimento, injeta os scripts do Google Analytics (ou Meta Pixel, etc.)
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-P8RW64Y59Y" strategy="afterInteractive" />
-<Script id="google-analytics" strategy="afterInteractive>
-      { window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
-        gtag('js', new Date());
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-P8RW64Y59Y"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        gtag('config', 'G-P8RW64Y59Y');
-      }
-</Script>
+  gtag('config', 'G-P8RW64Y59Y');
+</script>
     </>
   );
 }
